58a8bc466328e9b7ae749c2a58648d05379b364f
maverikarsh <arshhussein@outlook.com>
2021-11-26 08:24:28 +0100

    upload file feature

---
+++ Controllers/PhotoEditorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PhotoEngineService.DLL;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PhotoEngineService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoEditorController : ControllerBase
+    {
+        public IConfiguration _config;
+
+        public PhotoEditorController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public async Task<bool> UploadPhotoToBlob()
+        {
+            PhotoUploader objUploader = new PhotoUploader(_config);
+            bool isUploaded = false;
+            var httpRequest = HttpContext.Request;
+            //Upload Image
+            //var postedFile = httpRequest.["Image"];
+            //isUploaded = await objUploader.UploadPhoto(fileStream, fileName);
+            return isUploaded;
+        }
+
+    }
+}
---
+++ DLL/PhotoUploader.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace PhotoEngineService.DLL
+{
+    public class PhotoUploader
+    {
+        private readonly IConfiguration _config;
+
+        public PhotoUploader(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public string GetStorageConnection()
+        {
+            return _config.GetValue<string>("ConnectionStrings:azstorage");
+        }
+
+        public async Task<bool> UploadPhoto(Stream fileStream, string fileName)
+        {
+            BlobServiceClient blobSvcClient = new BlobServiceClient(GetStorageConnection());
+            string containerName = _config.GetValue<string>("BlobContainers:bmwPhotoContainer");
+            BlobContainerClient blobContainerClient = new BlobContainerClient(GetStorageConnection(), containerName);
+            blobContainerClient.CreateIfNotExists();
+            await blobContainerClient.UploadBlobAsync(fileName, fileStream);
+            return await Task<bool>.FromResult(true);
+        }
+    }
+}
